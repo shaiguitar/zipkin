e34883b5ce1f2ed048fb45934fc4d3422bd73646
Shai Rosenfeld <srosenfeld@engineyard.com>
2013-06-28 11:35:29 -0700

    test specing out

--- zipkin-gems/zipkin-tracer/test/test_helper.rb
+++ zipkin-gems/zipkin-tracer/test/test_helper.rb
@@ -35,6 +35,9 @@ module TestHelpers
       "made request to /third_app/end"
     end
 
+    get '/norpc' do
+    end
+
     get '/end' do
       "end"
     end
@@ -54,7 +57,6 @@ module TestHelpers
 
   class ThirdApp < Sinatra::Base
     def self.config; ZipkinConfig; end
-
     get '/continue' do
       TestHelpers.client.get(TestHelpers.url + "/first_app/end")
       "made request to /first_app/end"
@@ -65,22 +67,41 @@ module TestHelpers
 
   end
 
-  ## web of api requests to simulate: 
-  #
-  # /first_app/start
-  # => /second_app/continue
-  #   => /first_app/end
-  #   => /third_app/continue
-  #     => /first_app/end
-  #
-  # /first_app/simple
-  # => /third_app/end
-  #
   # main assertions need to do figure 2 in
   # http://static.googleusercontent.com/external_content/untrusted_dlcp/research.google.com/en/us/pubs/archive/36356.pdf
-  # they are: TODO
   #
- 
+  # SHARED:
+  # =======
+  #
+  # assert that we have client start, server recv, server send, client receive, http uri/method, annotations in spans.
+  #
+  ## WEB OF API REQUESTS TO SIMULATE: 
+  # =================================
+  #
+  # => /first_app/norpc
+  #
+  # assert a single span in a trace, with no parent id (but still has the shared attribute annotations as above)
+  #
+  # => /first_app/simple
+  #   => /third_app/end
+  #
+  # assert that all the annotations we have should coorelate to two spans: one span for '/simple' and another for '/end'
+  # assert trace id is the same for both spans and belong to it
+  # assert the first span has no parent id, second span has a parent id of the first.
+  #
+  # => /first_app/start
+  #   => /second_app/continue
+  #     => /first_app/end
+  #     => /third_app/continue
+  #       => /first_app/end
+  #  
+  # assert there are 5 spans in this one trace. all have same trace_id: ( num below really just a hex code)
+  # span: /first_app/start, parent_id=nil, span_id=1
+  # span: /second_app/continue, parent_id=1, span_id=2
+  # span: /first_app/end, parent_id=2, span_id=3
+  # span: /third_app/continue, parent_id=2, span_id=4
+  # span: /first_app/end, parent_id=4, span_id=5
+
   class MainApplication
     def self.app
       Rack::Builder.new do
