122f4e806853a3d18b2f101b940a0fa16af8b0de
Shai Rosenfeld <srosenfeld@engineyard.com>
2013-06-27 22:27:23 -0700

    api calls

--- zipkin-gems/zipkin-tracer/test/middleware_test.rb
+++ zipkin-gems/zipkin-tracer/test/middleware_test.rb
@@ -1,57 +1,6 @@
 require 'test/unit'
 require 'test_helper'
 
-class ZipkinConfig
-  def self.zipkin_tracer
-    {service_name: 'some_app', service_port: 9410, sample_rate: 1, scribe_server: "127.0.0.1:9410"}
-  end
-end
-
-class App < Sinatra::Base
-
-  def self.config
-    ZipkinConfig
-  end
-  get '/' do
-    'homepage'
-  end
-  get '/one' do
-    redirect "/prefix/two"
-  end
-  get '/three' do
-    "three"
-  end
-
-  get '/A' do
-    redirect '/prefix/B'
-  end
-end
-
-class AppTwo < Sinatra::Base
-
-  def self.config
-    ZipkinConfig
-  end
-
-  get '/two' do
-    redirect "/three"
-  end
-
-  get '/B' do
-    "B"
-  end
-end
-
-class MainApplication
-  def self.app
-    Rack::Builder.new do
-      map("/")    { use ZipkinTracer::RackHandler; run App }
-      map("/prefix/") { use ZipkinTracer::RackHandler; run AppTwo }
-    end
-  end
-end
-
-MainApplication.app
 
 class TestAdd < Test::Unit::TestCase
 
--- zipkin-gems/zipkin-tracer/test/test_helper.rb
+++ zipkin-gems/zipkin-tracer/test/test_helper.rb
@@ -3,3 +3,71 @@ require 'sinatra'
 require 'pry'
 require 'rack'
 require 'rack/client'
+
+module TestHelpers
+
+  def self.client
+    @client = Rack::Client.new(TestHelpers.url) do
+      run MainApplication.app
+    end
+  end
+
+  def self.url
+    "http://some.place"
+  end
+
+  class ZipkinConfig
+    def self.zipkin_tracer
+      {service_name: 'some_app', service_port: 9410, sample_rate: 1, scribe_server: "127.0.0.1:9410"}
+    end
+  end
+
+  class FirstApp < Sinatra::Base
+    def self.config; ZipkinConfig; end
+
+    get '/start' do
+      TestHelpers.client.get(TestHelpers.url + "/second_app/continue")
+    end
+
+    get '/end' do
+    end
+
+  end
+
+  class SecondApp < Sinatra::Base
+    def self.config; ZipkinConfig; end
+
+    get '/continue' do
+      TestHelpers.client.get(TestHelpers.url + "/first_app/end")
+      TestHelpers.client.get(TestHelpers.url + "/third_app/continue")
+    end
+
+  end
+
+  class ThirdApp < Sinatra::Base
+    def self.config; ZipkinConfig; end
+
+    get '/continue' do
+      TestHelpers.client.get(TestHelpers.url + "/first_app/end")
+    end
+
+  end
+
+  ## web of api requests to simulate: 
+  # /first_app/start
+  # => /second_app/continue
+  #   => /first_app/end
+  #   => /third_app/continue
+  #     => /first_app/end
+ 
+  class MainApplication
+    def self.app
+      Rack::Builder.new do
+        map("/first_app/")    { use ZipkinTracer::RackHandler; run FirstApp }
+        map("/second_app/") { use ZipkinTracer::RackHandler; run SecondApp }
+        map("/third_app/") { use ZipkinTracer::RackHandler; run ThirdApp }
+      end
+    end
+  end
+
+end
